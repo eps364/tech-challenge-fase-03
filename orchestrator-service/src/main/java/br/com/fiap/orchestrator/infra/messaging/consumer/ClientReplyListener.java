62b4de1e682a8651698995b1591290985c660842
Luiz Saraiva <luizsaraiva50@gmail.com>
2026-03-06 20:50:16 -0300

    info docker

---
+++ orchestrator-service/src/main/java/br/com/fiap/orchestrator/infra/messaging/consumer/ClientReplyListener.java
@@ -0,0 +1,16 @@
+package br.com.fiap.orchestrator.infra.messaging.consumer;
+
+import br.com.fiap.orquestrador.core.dto.MensagemFila;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientReplyListener {
+
+    @RabbitListener(queues = "Pedidos-Orquestrador.queue")
+    public void onMessage(MensagemFila mensagem) {
+        // aqui você chamaria um usecase do core para tratar resposta
+        // por enquanto, só log
+        System.out.println("Resposta de Pedidos: " + mensagem);
+    }
+}
--- orchestrator-service/src/main/java/br/com/fiap/orchestrator/infra/messaging/consumer/PedidosReplyListener.java
+++ orchestrator-service/src/main/java/br/com/fiap/orchestrator/infra/messaging/consumer/PedidosReplyListener.java
@@ -1,16 +1,23 @@
 package br.com.fiap.orchestrator.infra.messaging.consumer;
 
 import br.com.fiap.orchestrator.core.dto.MensagemFila;
+import br.com.fiap.orchestrator.core.usecase.enviar_para_pedidos.EnviarParaPedidosUseCase;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PedidosReplyListener {
 
+    private final EnviarParaPedidosUseCase enviarParaPedidosUseCase;
+
+    public PedidosReplyListener(EnviarParaPedidosUseCase enviarParaPedidosUseCase) {
+        this.enviarParaPedidosUseCase = enviarParaPedidosUseCase;
+    }
+
     @RabbitListener(queues = "Pedidos-Orquestrador.queue")
     public void onMessage(MensagemFila mensagem) {
-        // aqui você chamaria um usecase do core para tratar resposta
-        // por enquanto, só log
         System.out.println("Resposta de Pedidos: " + mensagem);
+
+        enviarParaPedidosUseCase.execute();
     }
 }
