439ac60a1f929441abd53a0abb82fdf9f3b44bea
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2026-03-20 23:43:56 -0300

    chore(auth): update dependencies, Dockerfile, and controller

--- auth-service/src/main/java/br/com/fiap/authservice/infra/web/controller/AuthController.java
+++ auth-service/src/main/java/br/com/fiap/authservice/infra/web/controller/AuthController.java
@@ -2,6 +2,7 @@ package br.com.fiap.authservice.infra.web.controller;
 
 import java.time.Instant;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
