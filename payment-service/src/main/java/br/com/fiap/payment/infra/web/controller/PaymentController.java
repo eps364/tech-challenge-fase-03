e7463e82b296afb05351e185e8253b301ee19ef9
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2026-03-20 23:43:58 -0300

    chore(payment): update dependencies, Dockerfile, and controllers

--- payment-service/src/main/java/br/com/fiap/payment/infra/web/controller/PaymentController.java
+++ payment-service/src/main/java/br/com/fiap/payment/infra/web/controller/PaymentController.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +13,6 @@ import br.com.fiap.payment.core.dto.PaymentRequest;
 import br.com.fiap.payment.core.dto.PaymentResponse;
 import br.com.fiap.payment.core.usecase.GetPaymentByOrderIdUseCase;
 import br.com.fiap.payment.infra.web.controller.api.PaymentAPI;
-import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/payments")
--- payment-service/src/main/java/br/com/fiap/payment/infra/web/controller/api/PaymentAPI.java
+++ payment-service/src/main/java/br/com/fiap/payment/infra/web/controller/api/PaymentAPI.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.fiap.payment.core.dto.PaymentResponse;
 import io.swagger.v3.oas.annotations.Operation;
