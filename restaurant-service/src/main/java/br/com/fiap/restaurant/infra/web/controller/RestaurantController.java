29b08ebc73f15a7dc1916f6a86823c1af13eca61
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2026-03-20 23:43:59 -0300

    chore(restaurant): update dependencies, Dockerfile, and controller

--- restaurant-service/src/main/java/br/com/fiap/restaurant/infra/web/controller/RestaurantController.java
+++ restaurant-service/src/main/java/br/com/fiap/restaurant/infra/web/controller/RestaurantController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
